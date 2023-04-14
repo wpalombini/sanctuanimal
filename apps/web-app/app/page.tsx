376799b24133aadcfeb747489dd42e49f5fafedb
Walter Palombini <walterpalombini@gmail.com>
2023-04-14 23:14:47 +1000

    Prisma (orm package) implementation

--- apps/web-app/app/page.tsx
+++ apps/web-app/app/page.tsx
@@ -1,10 +1,26 @@
+import { PrismaClient } from '@sanctuanimal/orm';
 import { Button } from 'ui';
 
 const getUser = async () =>
   await fetch(`${process.env.BASE_URL}/api/user`, { cache: 'no-store' }).then((res) => res.json());
 
+const test = async () => {
+  const prisma = new PrismaClient();
+  const result = await prisma.user.findUnique({
+    where: { email: 'walter@palombini.com' },
+  });
+
+  console.log(result);
+};
+
 export default async function Web() {
   const user = await getUser();
+
+  try {
+    await test();
+  } catch (error) {
+    console.log('err', error);
+  }
 
   return (
     <div>
--- apps/web-app/next.config.js
+++ apps/web-app/next.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+// https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-monorepo
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
 
 module.exports = {
   reactStrictMode: true,
@@ -7,5 +9,12 @@ module.exports = {
   experimental: {
     appDir: true,
     outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
   },
 };
