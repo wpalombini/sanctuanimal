23b8327cc356ecbacbc3ff8e0827b6b8a263caef
Walter Palombini <walterpalombini@gmail.com>
2023-04-20 17:20:43 +1000

    Custom theme settings: background colour

--- packages/ui/components/theme/index.ts
+++ packages/ui/components/theme/index.ts
@@ -4,5 +4,23 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 import { createTheme } from '@mui/material';
+import { blue, deepOrange } from '@mui/material/colors';
 
-export const theme = createTheme();
+export const theme = createTheme({
+  palette: {
+    primary: blue,
+    secondary: deepOrange,
+    background: {
+      default: '#eee',
+    },
+  },
+  components: {
+    MuiLink: {
+      styleOverrides: {
+        root: {
+          textDecoration: 'none',
+        },
+      },
+    },
+  },
+});
