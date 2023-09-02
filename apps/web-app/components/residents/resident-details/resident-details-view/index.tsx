3bd1528ca3f0195b6d3335026223fe4b62ea712a
Walter Palombini <walterpalombini@gmail.com>
2023-09-02 12:54:07 +1000

    Updated Resident Details style card

--- apps/web-app/components/residents/resident-details/resident-details-view/index.tsx
+++ apps/web-app/components/residents/resident-details/resident-details-view/index.tsx
@@ -1,4 +1,4 @@
-import { CardContent } from '@sanctuanimal/ui';
+import { Avatar, Box, CardContent, Typography } from '@sanctuanimal/ui';
 
 import { DisplayField } from '@/components/ui';
 import { getLabelForGenderValue } from '@/lib/utils';
@@ -9,15 +9,38 @@ type ResidentDetailsViewProps = {
   residentData?: ResidentDetailsForm;
 };
 
+const ResidentDetailsContainer = ({ children }: { children: React.ReactNode }) => (
+  <Box sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' }, gap: { xs: 1, md: 5 } }}>
+    {children}
+  </Box>
+);
+
 const ResidentDetailsView = ({ residentData }: ResidentDetailsViewProps) => {
   return (
-    <CardContent sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-      <DisplayField label="Name" value={residentData?.name} />
-      <DisplayField label="Species" value={residentData?.species} />
-      <DisplayField label="Breed" value={residentData?.breed} />
-      <DisplayField label="Gender" value={getLabelForGenderValue(residentData?.gender)} />
-      <DisplayField label="DOB" value={residentData?.dateOfBirth as string} />
-      <DisplayField label="Bio" value={residentData?.bio} />
+    <CardContent sx={{ display: 'flex', flexDirection: 'column', gap: 5 }}>
+      <Box sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' } }}>
+        <Avatar
+          alt="SanctuAnimal logo"
+          src="/generic-profile.webp"
+          sx={{ height: { xs: 50, md: 200 }, width: { xs: 50, md: 200 } }}
+        />
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, padding: { xs: 0, md: 5 } }}>
+          <Typography variant="h4" noWrap>
+            {residentData?.name}
+          </Typography>
+          <Typography variant="subtitle1">{residentData?.bio}</Typography>
+        </Box>
+      </Box>
+      <ResidentDetailsContainer>
+        <ResidentDetailsContainer>
+          <DisplayField label="Species" value={residentData?.species} />
+          <DisplayField label="Breed" value={residentData?.breed} />
+        </ResidentDetailsContainer>
+        <ResidentDetailsContainer>
+          <DisplayField label="Gender" value={getLabelForGenderValue(residentData?.gender)} />
+          <DisplayField label="DOB" value={residentData?.dateOfBirth as string} />
+        </ResidentDetailsContainer>
+      </ResidentDetailsContainer>
     </CardContent>
   );
 };
