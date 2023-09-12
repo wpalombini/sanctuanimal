ce94977c740f1aa7811872ee275d1cab67a355e7
Walter Palombini <walterpalombini@gmail.com>
2023-09-12 16:20:09 +1000

    Using docker compose for postgres and seeding dev env

--- packages/orm/README.md
+++ packages/orm/README.md
@@ -9,4 +9,32 @@ pnpm prisma migrate dev --name init
 
 Where `init` is the name of the migration file. Choose a name related to the changes you are making on the schema.
 
+## Running the database
+
+```
+pnpm db:dev:up
+```
+
+The above command will run docker compose up.
+
+```
+pnpm prisma migrate dev
+```
+
+The above command will migrate the db schema.
+
+```
+pnpm db:dev:seed
+```
+
+The above command will seed the database.
+
+## Resetting the database
+
+```
+docker compose down --volumes
+```
+
+The above command will remove the docker volume that holds the database.
+
 For more details, [see this link](https://www.prisma.io/docs/concepts/components/prisma-migrate/migrate-development-production#production-and-testing-environments).
---
+++ packages/orm/prisma/seed.ts
@@ -0,0 +1,52 @@
+import { PrismaClient } from '../generated/index.js';
+
+const prisma = new PrismaClient({
+  log: ['query'],
+});
+
+const main = async () => {
+  const user1DisplayName = 'Peach Orange';
+  const user1Email = 'peach.orange.733@example.com';
+  const user1ExternalId = 'cYufyazkAeXV70lJxrieWV6TW5RL';
+  const user1Sanctuary1Name = 'Waltz Sanctuary Seed';
+  const user1Sanctuary1Contact = 'Waltz Sanctuary Contact Seed';
+
+  const isDBSeeded = await prisma.user.findFirst({
+    where: { externalId: user1ExternalId },
+  });
+
+  if (isDBSeeded) {
+    return;
+  }
+
+  const account1 = await prisma.user.create({
+    data: {
+      externalId: user1ExternalId,
+      email: user1Email,
+      name: user1DisplayName,
+    },
+  });
+
+  const sanctuary1Account1 = await prisma.sanctuary.create({
+    data: {
+      name: user1Sanctuary1Name,
+      contact: user1Sanctuary1Contact,
+    },
+  });
+
+  await prisma.userSanctuary.create({
+    data: {
+      userId: account1.id,
+      sanctuaryId: sanctuary1Account1.id,
+    },
+  });
+};
+
+try {
+  await main();
+} catch (e) {
+  console.error(e);
+  process.exit(1);
+} finally {
+  await prisma.$disconnect();
+}
